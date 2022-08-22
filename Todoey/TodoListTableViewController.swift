0085d3e75a19e8b5bbeaf78aa901a7bdc18dcfb1
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-23 00:32:33 +0300

    make didSelectRowAt method

--- Todoey/TodoListTableViewController.swift
+++ Todoey/TodoListTableViewController.swift
@@ -14,12 +14,7 @@ class TodoListTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
     }
 
     // MARK: - Table view data source
@@ -74,6 +69,12 @@ class TodoListTableViewController: UITableViewController {
     }
     */
 
+    // MARK: - Table view delegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(itemArray[indexPath.row])
+    }
+    
+    
     /*
     // MARK: - Navigation
 
