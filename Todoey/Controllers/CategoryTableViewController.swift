214ebfe68e227f42d9e9ef5695bb269b3d996508
Alexey Suslov <suslov.aleksey.oo7@gmail.com>
2022-08-25 00:37:09 +0300

    add new CategoryTableViewController for the list of categories add new Category Entity to the Core Data Base and make relationships with TodoItem Entity

---
+++ Todoey/Controllers/CategoryTableViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryTableViewController.swift
+//  Todoey
+//
+//  Created by ALEKSEY SUSLOV on 25.08.2022.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+    
+}
